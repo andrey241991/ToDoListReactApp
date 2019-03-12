dc52a70fab8c7e5cec730cc5a6002518ca4258eb
Your Name <droni2442@gmail.com>
2019-03-12 11:50:44 +0200

    add input component

---
+++ src/Components/Input/Input.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import "./Input.css";
+
+class Input extends Component {
+  state = {
+    title: ""
+  };
+
+  addNewTask = () => {
+    let newTask = {
+      title: this.state.title,
+      data: +new Date(),
+      isActive: true,
+      isCompleted: false,
+      isSelected: false,
+      isEdit: false
+    };
+    this.props.handlerFromParantAddNewTask(newTask);
+  };
+
+  handleInputChange = event => {
+    this.setState({ title: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="Input">
+        <input
+          className="Input-field"
+          maxLength="40"
+          onChange={this.handleInputChange}
+        />
+        <button className="Button-add" onClick={this.addNewTask}>
+          ADD
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Input;
