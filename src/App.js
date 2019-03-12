d16103f0ff468bf90e53237a2a77aafd1cdb3d0b
Your Name <droni2442@gmail.com>
2019-03-12 11:46:41 +0200

    add app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Input from "./Components/Input/Input";
+import List from "./Components/List/List";
+import Selected from "./Components/Selected/Selected";
+import Filters from "./Components/Filters/Filters";
 
 class App extends Component {
+
+  state = {
+    listOfTasks: [],
+    sortedListOfTasks: [],
+    showSortedTasks:false
+  };
+
+  addNewTaskToList = (newTask) => {
+    this.setState({
+      listOfTasks: [...this.state.listOfTasks, newTask]
+    });
+  }
+
+  changeListOfTasks = (newListOfTasks) => {
+    this.setState({
+      listOfTasks: newListOfTasks
+    });
+  }
+
+  showNotSortedListOfTasks = () =>{
+    this.setState({
+      showSortedTasks:false
+    })
+  }
+
+  addSortedListOfTasks = (sortedListOfTasks) => {
+    this.setState({
+      sortedListOfTasks: sortedListOfTasks
+    });
+
+    this.setState({
+      showSortedTasks:true
+    });
+  }
+
   render() {
+    let  listOfTasks;
+    if (this.state.showSortedTasks) {
+      listOfTasks = this.state.sortedListOfTasks;
+    } else {
+      listOfTasks = this.state.listOfTasks;
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <Header />
+          <Input handlerFromParantAddNewTask={this.addNewTaskToList} />
+          <Selected
+            listOfTasks={this.state.listOfTasks}
+            fromParentChangeListOfTasks={this.changeListOfTasks}
+          />
+          <div className="ListAndSortParent">
+            <Filters
+              listOfTasks={this.state.listOfTasks}
+              fromParentAddSortedListOfTasks={this.addSortedListOfTasks}
+              fromParentShowNotSortedListOfTasks={this.showNotSortedListOfTasks}
+            />
+            <List
+              listOfTasks={listOfTasks}
+              fromParentChangeListOfTasks={this.changeListOfTasks}
+            />
+          </div>
+        </div>
       </div>
     );
   }
