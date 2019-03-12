92cbf12bb1e449c2bd70c791fa0199390350c471
Your Name <droni2442@gmail.com>
2019-03-12 11:56:36 +0200

    add selected component

---
+++ src/Components/Selected/Selected.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class Selected extends Component {
+  
+  setChecked = checked => {
+    for (let value of this.props.listOfTasks) {
+      value.isSelected = checked;
+    }
+    this.props.fromParentChangeListOfTasks(this.props.listOfTasks);
+  };
+
+  deleteSelected = () => {
+    let newTasks = [];
+    for (let value of this.props.listOfTasks) {
+      if (!value.isSelected) {
+        newTasks.push(value);
+      }
+    }
+    this.props.fromParentChangeListOfTasks(newTasks);
+  };
+
+  render() {
+    return (
+      <div className="Selected">
+        <button onClick={() => this.setChecked(true)}>CHECK ALL</button>
+        <button onClick={() => this.setChecked(false)}>UNCHECK ALL</button>
+        <button onClick={this.deleteSelected}>DELETE SELECTED</button>
+      </div>
+    );
+  }
+}
+
+export default Selected;
